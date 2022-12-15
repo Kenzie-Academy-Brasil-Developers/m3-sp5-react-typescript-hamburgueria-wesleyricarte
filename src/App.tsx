6650a7ca854a6875ba9a8426d09260f57f7417a6
Wesley Ricarte <wesley.ricarte97@gmail.com>
2022-12-15 17:20:08 -0300

    feat/starting-project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello world, my name is Wesley and that's my first ever TypeScript project</h1>
+    </div>
+  );
+}
+
+export default App;
